6480b2664c748fe0ed20768e2222d3532ae9b4ed
Stephen <hellostephenwm@gmail.com>
2025-03-07 17:57:49 -0500

    chore: initial

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -1,21 +1,36 @@
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-	{ ignores: ['dist'] },
+	{ ignores: ['dist', 'node_modules'] },
 	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			...tseslint.configs.stylistic,
+		],
 		files: ['**/*.{ts,tsx}'],
 		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
+			sourceType: 'module',
+			ecmaVersion: 'latest',
+			parser: tseslint.parser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				tsConfigRootDir: './',
+			},
+			globals: { ...globals.browser, ...globals.node, ...globals.es2021 },
 		},
 		plugins: {
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
+			'@typescript-eslint': tseslint.plugin,
+			prettier: eslintPluginPrettier,
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
@@ -23,6 +38,56 @@ export default tseslint.config(
 				'warn',
 				{ allowConstantExport: true },
 			],
+			// Prettier rules
+			'prettier/prettier': 'error',
+
+			// General rules
+			'linebreak-style': ['error', 'unix'],
+			'max-len': [
+				'error',
+				{
+					code: 80,
+					ignoreStrings: true,
+					ignoreTemplateLiterals: true,
+					ignoreComments: true,
+				},
+			],
+			indent: ['error', 'tab', { SwitchCase: 1 }],
+			quotes: ['error', 'single'],
+			semi: ['error', 'always'],
+			eqeqeq: 'error',
+			'arrow-spacing': ['error', { before: true, after: true }],
+			'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+			'object-curly-spacing': ['error', 'always'],
+			'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+			'no-trailing-spaces': 'error',
+			'no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					args: 'after-used',
+					ignoreRestSiblings: false,
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
+	},
+
+	// Apply prettier config last (to disable conflicting rules)
+	{
+		name: 'Prettier Config',
+		rules: eslintConfigPrettier.rules,
+	},
+
+	// Language options & globals
+	{
+		languageOptions: {
+			parserOptions: { sourceType: 'module', ecmaVersion: 'latest' },
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2021,
+			},
 		},
 	}
 );
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,10 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,9 +16,11 @@ export default defineConfig({
 			'@/components/*': path.resolve(__dirname, './src/components/*'),
 		},
 	},
+	base: './', // Ensures assets load correctly
 	build: {
-		cssCodeSplit: true,
+		cssCodeSplit: false,
 		sourcemap: false,
-		manifest: false,
+		manifest: true,
+		outDir: 'dist',
 	},
 });
