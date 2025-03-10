be896c6ef3b28ee0a0fec18c95d2bde91ca46490
Stephen <hellostephenwm@gmail.com>
2025-03-10 01:49:20 -0400

    chore: list of allowed origins for CORS

---
+++ packages/api/src/config/allowed-origins.js
@@ -0,0 +1,9 @@
+// Array of whitelisted origins
+const whitelist = [
+	'http://localhost:8080',
+	'https://fullstack-auth-client.vercel.app/',
+	'https://fullstack-auth-api.fly.dev/api/',
+];
+
+// Exporting whitelist
+export default whitelist;
