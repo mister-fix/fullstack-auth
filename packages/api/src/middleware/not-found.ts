f8e4a7c6597bba7099423bd0d767f9be71098729
Stephen <hellostephenwm@gmail.com>
2025-03-10 01:58:44 -0400

    feat: 404 not found handling middleware

---
+++ packages/api/src/middleware/not-found.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+
+import logger from '@/utils/logger';
+
+/**
+ * Middleware to handle 404 Not Found errors.
+ */
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+	// Create a detailed error message
+	const errorMessage = `The requested resource '${req.originalUrl}' was not found 🤷`;
+
+	// Log the error for debugging purposes
+	logger.error(`404 Not Found: ${errorMessage}`);
+
+	// Set the response status to 404
+	res.status(404);
+
+	// Pass the error to the next middleware
+	next({
+		message: errorMessage,
+		statusCode: 404,
+		stack:
+			process.env.NODE_ENV === 'production' ? undefined : new Error().stack,
+	});
+};
